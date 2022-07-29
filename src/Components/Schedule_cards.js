cc395ec833958e1e903741a57aeb9dfb88a42fc8
Divyansh3021 <divyansh3021@gmail.com>
2022-07-30 01:31:57 +0530

    bg balls added

--- src/Components/Schedule_cards.js
+++ src/Components/Schedule_cards.js
@@ -9,27 +9,27 @@ export default function Schedule_cards(props) {
 
   return (
         <div className="programs">
-            <div className="program"><h3 className='event' style={{backgroundColor:color}}>Event</h3>
+            <div className="program" style={{ boxShadow: "0px 5px 10px 0px "+color}}><h3 className='event' style={{backgroundColor:color}}>Event</h3>
             <h2 className='event_title data'>Data and Time</h2>
             <h2 className='event_title data'>Description</h2>
             <div className="more_info" style={{backgroundColor:color}}>More Info</div>
             </div>
             
 
-            <div className="program"><h3 className='event' style={{backgroundColor:color}}>Event</h3>
+            <div className="program" style={{ boxShadow: "0px 5px 10px 0px "+color}}><h3 className='event' style={{backgroundColor:color}}>Event</h3>
             <h2 className='event_title data'>Data and Time</h2>
             <h2 className='event_title data'>Description</h2>
-            <div className="more_info">More Info</div>
+            <div className="more_info" style={{backgroundColor:color}}>More Info</div>
             </div>
-            <div className="program"><h3 className='event' style={{backgroundColor:color}}>Event</h3>
+            <div className="program" style={{ boxShadow: "0px 5px 10px 0px "+color}}><h3 className='event' style={{backgroundColor:color}}>Event</h3>
             <h2 className='event_title data'>Data and Time</h2>
             <h2 className='event_title data'>Description</h2>
-            <div className="more_info">More Info</div>
+            <div className="more_info" style={{backgroundColor:color}}>More Info</div>
             </div>
-            <div className="program"><h3 className='event' style={{backgroundColor:color}}>Event</h3>
+            <div className="program" style={{ boxShadow: "0px 5px 10px 0px "+color}}><h3 className='event' style={{backgroundColor:color}}>Event</h3>
             <h2 className='event_title data'>Data and Time</h2>
             <h2 className='event_title data'>Description</h2>
-            <div className="more_info">More Info</div>
+            <div className="more_info" style={{backgroundColor:color}}>More Info</div>
             </div>
         </div>
   )

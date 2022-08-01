7db1f4b9f619d5a634588459fc9c76cb4138d9d0
Divyansh3021 <divyansh3021@gmail.com>
2022-08-01 17:00:33 +0530

    navbar fixed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,13 +2,21 @@ import React from 'react'
 import './Assets/Images/CSS/Navbar.css'
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className='navbar' id='navbar' style={{display : "flex", justifyContent: "center", width: "100%"}}>
+      <div className="nav-svg" style={{display: "flex", justifyContent: "center", width: "100%" }}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 197.76 27.94" width="40%">
+        <defs>
+        </defs>
+        <g class="a">
+          <path class="b" d="M21.12,106.08s4.48.64,9.36,8.16c0,0,10.74,19.68,22.08,19.68H186.24s8.83,2.19,20.16-14.16c0,0,6.56-11.2,12.48-13.68Z" transform="translate(-21.12 -106.08)"/>
+        </g>
+      </svg>
+      </div>
       <div className="mainNav">
         <div className="nav-con"><h4>About</h4></div>
         <div className="nav-con"><h4>Event</h4></div>
-        <div className="nav-con"><img src={require('./Assets/Images/aswd.png')} alt=""  style={{height:"50%", width:"50%"}} /></div>
+        <div className="nav-con" style={{display: "flex", justifyContent: "center"}}><img src={require('./Assets/Images/aswd.png')} alt=""  style={{width:"100%"}} /></div>
         <div className="nav-con"><h4>Schedule</h4></div>
-        <div className="nav-con"><h4>Reach us</h4></div>
+        <div className="nav-con"><h4>Contact</h4></div>
       </div>
     </div>
   )

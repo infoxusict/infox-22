b141925a1669a05b0c0dc21fbb0225c75f05d6ee
ANSH GOYAL <72507723+Ansh1693@users.noreply.github.com>
2022-08-01 16:21:15 +0530

    Legacy responsive done

--- src/Components/legacy.js
+++ src/Components/legacy.js
@@ -28,7 +28,34 @@ const sliderData3 = [
   "./Assets/Images/legacy/S3-05.JPG",
   "./Assets/Images/legacy/S3-06.JPG",
 ];
+
 export default function Legacy() {
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const [height, setHeight] = React.useState(window.innerHeight);
+  const useViewport = () => {
+    React.useEffect(() => {
+      const handleWindowResize = () => {
+        setWidth(window.innerWidth);
+
+        setHeight(window.innerHeight);
+      };
+
+      window.addEventListener("resize", handleWindowResize);
+      return () => window.removeEventListener("resize", handleWindowResize);
+    }, []);
+
+    // Return both the height and width
+    return { width, height };
+  };
+
+  const MyComponent = () => {
+    const { width, height } = useViewport();
+    // const { height } = useHeight();
+    const breakpoint = 650;
+
+    return width < breakpoint ? <MobileComponent /> : <DesktopComponent />;
+  };
+
   const count_iteration = () => {
     let valueDisplays = document.querySelectorAll(".num");
 
@@ -55,131 +82,121 @@ export default function Legacy() {
     });
   };
 
-  function load_balls() {
-    const colors = ["#3CC157", "#b43232"];
-
-    if (window.innerWidth < 721) {
-      document.getElementById("main_con").classList.remove("main_con");
-    }
-
-    const numBalls = 50;
-    const balls = [];
-    // var offset_height = document.getElementById("programs").offsetHeight
-
-    for (let i = 0; i < numBalls; i++) {
-      let ball = document.createElement("div");
-      ball.classList.add("ball");
-      ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-      ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-      ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-      ball.style.transform = `scale(${Math.random()})`;
-      ball.style.width = `${Math.random()}em`;
-      ball.style.height = ball.style.width;
-
-      balls.push(ball);
-      document.body.append(ball);
-    }
-
-    // Keyframes
-    balls.forEach((el, i, ra) => {
-      let to = {
-        x: Math.random() * (i % 2 === 0 ? -11 : 11),
-        y: Math.random() * 12,
-      };
-
-      let anim = el.animate(
-        [
-          { transform: "translate(0, 0)" },
-          { transform: `translate(${to.x}rem, ${to.y}rem)` },
-        ],
-        {
-          duration: (Math.random() + 1) * 2000, // random duration
-          direction: "alternate",
-          fill: "both",
-          iterations: Infinity,
-          easing: "ease-in-out",
-        }
-      );
-    });
-  }
-
-  return (
-    <>
-      <div className="legacy" onLoad={load_balls}>
-        <div className="navbar"></div>
-        <div className="legacy__hero">
-          <div className="legacy__hero-left">
-            {/* <div className="left__background">
+  const DesktopComponent = () => {
+    return (
+      <div className="legacy__hero">
+        <div className="legacy__hero-left">
+          {/* <div className="left__background">
               <div id="stars"></div>
               <div id="stars2"></div>
               <div id="stars3"></div>
             </div> */}
-            <div className="hero__left-name">
-              <span>InfoXpression</span>
-            </div>
-            <div className="hero__left-head">
-              <div className="hero__heading-svg">
-                <svg
-                  height="120"
-                  stroke="rgb(0, 255, 60)"
-                  stroke-width="3"
-                  className="text-line"
-                  width="80%"
-                >
-                  <text
-                    x="50%"
-                    // fill="black"
-                    dominant-baseline="middle"
-                    text-anchor="middle"
-                    y="50%"
-                    data-text="LEGACY"
-                    className="heading"
-                  >
-                    LEGACY
-                  </text>
-                </svg>
-              </div>
-            </div>
-            <div className="hero__left-desc">
-              <span>
-                Lorem ipsum is placeholder text commonly used in the graphic,
-                print, and publishing industries for previewing layouts and
-                visual mockups.
-              </span>
-            </div>
+          <div className="hero__left-name">
+            <span>InfoXpression</span>
           </div>
-          <div className="legacy__hero-right">
-            <div className="legacy__hero-sliders">
-              <div className="slider-4" onMouseEnter={count_iteration}>
-                <span className="num" data-val="16">
-                  16
-                </span>
-              </div>
-              <Slider
-                sliderData={sliderData1}
-                gridClass="slider-3"
-                width="20"
-                height="22.5"
-                xSlide="reverse"
-              />
-              <Slider
-                sliderData={sliderData2}
-                gridClass="slider-2"
-                width="30"
-                height="45"
-                ySlide="true"
-              />
-              <Slider
-                sliderData={sliderData3}
-                gridClass="slider-1"
-                width="50"
-                height="45"
-              />
-            </div>
+          <div className="hero__left-head  glitch">
+            <span className="" data-text="LEGACY">
+              LEGACY
+            </span>
+          </div>
+          <div className="hero__left-desc">
+            <span>
+              Lorem ipsum is placeholder text commonly used in the graphic,
+              print, and publishing industries for previewing layouts and visual
+              mockups.
+            </span>
           </div>
         </div>
-        {/* <Slider sliderData=sliderData
-        width="60vw" height="50vh" ySlide="false" /> */}
+        <div className="legacy__hero-right">
+          <div className="legacy__hero-sliders">
+            <div className="slider-4" onMouseEnter={count_iteration}>
+              <span className="num" data-val="16">
+                16
+              </span>
+            </div>
+            <Slider
+              sliderData={sliderData1}
+              gridClass="slider-3"
+              width="20"
+              height="22.5"
+              xSlide="reverse"
+            />
+            <Slider
+              sliderData={sliderData2}
+              gridClass="slider-2"
+              width="30"
+              height="45"
+              ySlide="true"
+            />
+            <Slider
+              sliderData={sliderData3}
+              gridClass="slider-1"
+              width="50"
+              height="45"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const MobileComponent = () => {
+    return (
+      <div className="legacy__hero">
+        <div className="legacy__hero-left">
+          <div className="hero__left-name">
+            <span>InfoXpression</span>
+          </div>
+          <div className="hero__left-head glitch">
+            <span className="" data-text="LEGACY">
+              LEGACY
+            </span>
+          </div>
+          {/* <div className="hero__left-desc">
+            <span>
+              ggggsum is placeholder text commonly used in the graphic, print,
+              and publishing industries for previewing layouts and visual
+              mockups.
+            </span>
+          </div> */}
+        </div>
+        <div className="legacy__hero-right">
+          <div className="legacy__hero-sliders">
+            <div className="slider-4" onMouseEnter={count_iteration}>
+              <span className="num" data-val="16">
+                16
+              </span>
+            </div>
+            <Slider
+              sliderData={sliderData1}
+              gridClass="slider-3"
+              width="40"
+              height="25"
+              xSlide="reverse"
+            />
+            <Slider
+              sliderData={sliderData2}
+              gridClass="slider-2"
+              width="60"
+              height="50"
+              ySlide="true"
+            />
+            <Slider
+              sliderData={sliderData3}
+              gridClass="slider-1"
+              width="100"
+              height="50"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  };
+  return (
+    <>
+      <div className="legacy">
+        <div className="navbar1"></div>
+        <MyComponent />
         <About />
       </div>
     </>

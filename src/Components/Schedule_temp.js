f8a00254730911349a1a14db832b9ad9d1873dcc
Kartik <kartikkc397@outlook.com>
2022-08-07 17:30:32 +0530

    Footer Fixes

---
+++ src/Components/Schedule_temp.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import MatrixRain from "./MatrixRain";
+
+const Schedule_temp = () => {
+    return(
+        <>
+        <MatrixRain />
+        <div className="Wrap-all">
+            <div className="heading-main">
+                <h1 className="see_ya_soon">To be Revealed</h1>
+                
+            </div>
+
+        </div>
+        </>
+    )
+}
+export default Schedule_temp; 

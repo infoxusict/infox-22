6d7fda433a82fcaa001f413bb3ab437d055aadfe
Divyansh3021 <divyansh3021@gmail.com>
2022-08-19 11:48:52 +0530

    Banner added 2

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -4,7 +4,7 @@ import "./Assets/Images/CSS/Banner.css";
 export default function Banner() {
   return (
     <div className='glass-bg'>
-        <h3 className='banner-text'>Our Campus Ambassador program is live. <a href="https://forms.gle/ntyctv9QwoVpzf8w7" className='form-link'>Click Here to Register</a></h3>
+        <h3 className='banner-text'>Our Campus Ambassador program is live. <a href="https://forms.gle/ntyctv9QwoVpzf8w7" target="_blank" className='form-link'>Click Here to Register</a></h3>
     </div>
   )
 }

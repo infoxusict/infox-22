648533c8ee24816f62b5d58bd858267abb2027c7
ANSH GOYAL <72507723+Ansh1693@users.noreply.github.com>
2022-10-04 23:30:12 +0530

    Added Modal window

---
+++ src/Components/Modal.js
@@ -0,0 +1,27 @@
+import React, { useRef, useEffect, useState } from "react";
+import Modal1 from "./Modal1";
+import "./Assets/Images/CSS/modal.css";
+
+// How to use:
+// Sample <Modal data={data} buttonName={"Register Now!"} />
+// data format =[{question: "", placeholder: ""}]
+//can add more data on req
+// use modal.js dont use modal1.js (it is a comp linked to modal.js)
+
+export default function Modal({ data, buttonName }) {
+  const [openModal, setOpenModal] = useState(false);
+  return (
+    <div>
+      <button
+        className="show-modal"
+        onClick={() => {
+          setOpenModal(true);
+          console.log("Changed to", openModal);
+        }}
+      >
+        {buttonName}
+      </button>
+      {openModal && <Modal1 closeModal={setOpenModal} data={data} />}
+    </div>
+  );
+}
---
+++ src/Components/Modal1.js
@@ -0,0 +1,72 @@
+export default function Modal1({ closeModal, data }) {
+  // const listItems = data.map(({ question }) => {
+  //   <div>
+  //     <label htmlFor={question}>{question}</label>
+  //     <input type="text" name={question}></input>;
+  //   </div>;
+  // });
+
+  const listItems = () => {
+    return (
+      <div>
+        {data.map((item, i) => (
+          <div>
+            <label htmlFor="Github">{item.question}</label>
+            <input
+              type="text"
+              name={item.question}
+              placeholder={item.placeholder}
+            ></input>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <div className="modal">
+        <button
+          className="close-modal"
+          onClick={() => {
+            console.log(data);
+            closeModal(false);
+          }}
+        >
+          &times;
+        </button>
+        <form action="">
+          {listItems()}
+          {/* {data.map((item, i) => {
+            // <div>
+            <div>
+              <label htmlFor="Github">{item}</label>
+              <input type="text" name="Github" placeholder="xyz123"></input>
+            </div>;
+            {
+              /* <div>
+                <label htmlFor="Github">Github</label>
+                <input type="text" name="Github" placeholder="xyz123"></input>
+              </div>
+              <div>
+                <label htmlFor="Github">Github</label>
+                <input type="text" name="Github" placeholder="xyz123"></input>
+              </div> 
+            }
+            // </div>;
+          })} */}
+
+          <div className="sb-modal">
+            <button className="submit-modal">Submit</button>
+          </div>
+        </form>
+      </div>
+      <div
+        class="overlay"
+        onClick={() => {
+          closeModal(false);
+        }}
+      ></div>
+    </>
+  );
+}

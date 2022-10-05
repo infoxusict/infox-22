8d277a9d559e13b6097785496503cad9c48b0538
Suraj Mishra <59327661+suraj-git07@users.noreply.github.com>
2022-10-06 01:42:39 +0530

    event card updated

--- src/Components/EventProfile.js
+++ src/Components/EventProfile.js
@@ -2,31 +2,64 @@ import React from "react";
 import "./Assets/Images/CSS/EventProfile.css";
 
 const EventCardProfile = () => {
-    return (
+  return (
+    <div className="programs" id="programs">
+      <div id="#Ccard" className="Ccard">
+        <div className="EventHeading">Event Name</div>
+        <div className="Eventtime"> 12 Nov 2022</div>
+        <div className="layer">
+          <div className="teams">
+            <div className="teamname">
+              <h3>player1 (leader)</h3>
 
-        <div className="programs" id='programs'>
-            <div className="Ccard">
-              <div className="EventHeading">Event Name</div>
-              <div className="Eventtime"> 12 Nov 2022</div>
-              <div className="layer">
-                <div className="teams">
-                    <div className="teamname">
-                    <h3>player1 (leader)</h3>
-                
-                    <h3>player2</h3>
-                
-                    <h3>player3</h3>
-                
-                    <h3>player4</h3>
-                    </div>
-                <button className="buttonn">more Info</button>
-                </div>
-              </div>
+              <h3>player2</h3>
+
+              <h3>player3</h3>
+
+              <h3>player4</h3>
             </div>
-            
+            <button className="button-49">more Info</button>
+          </div>
         </div>
+      </div>
+      <div id="#Ccard" className="Ccard">
+        <div className="EventHeading">Event Name</div>
+        <div className="Eventtime"> 12 Nov 2022</div>
+        <div className="layer">
+          <div className="teams">
+            <div className="teamname">
+              <h3>player1 (leader)</h3>
 
-    )
-}
+              <h3>player2</h3>
+
+              <h3>player3</h3>
+
+              <h3>player4</h3>
+            </div>
+            <button className="button-49">more Info</button>
+          </div>
+        </div>
+      </div>
+      <div id="#Ccard" className="Ccard">
+        <div className="EventHeading">Event Name</div>
+        <div className="Eventtime"> 12 Nov 2022</div>
+        <div className="layer">
+          <div className="teams">
+            <div className="teamname">
+              <h3>player1 (leader)</h3>
+
+              <h3>player2</h3>
+
+              <h3>player3</h3>
+
+              <h3>player4</h3>
+            </div>
+            <button className="button-49">more Info</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default EventCardProfile;

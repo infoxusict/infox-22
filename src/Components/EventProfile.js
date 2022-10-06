ffcbb9da175d5022446d6e7ee16c8edf70059716
Shivesh Nandan <kshivesh929@gmail.com>
2022-10-06 16:22:10 +0530

    new profile

--- src/Components/EventProfile.js
+++ src/Components/EventProfile.js
@@ -4,64 +4,104 @@ import "./Assets/Images/CSS/EventProfile.css";
 const EventCardProfile = () => {
     return (
 
-        <div className="programs" id='programs'>
-            <div id="#Ccard" className="Ccard">
-        <div className="EventHeading">Event Name</div>
-        <div className="Eventtime"> 12 Nov 2022</div>
-        <div className="layer">
-          <div className="teams">
-            <div className="teamname">
-              <h3>player1 (leader)</h3>
+      //   <div className="programs" id='programs'>
+      //       <div id="#Ccard" className="Ccard">
+      //   <div className="EventHeading">Event Name</div>
+      //   <div className="Eventtime"> 12 Nov 2022</div>
+      //   <div className="layer">
+      //     <div className="teams">
+      //       <div className="teamname">
+      //         <h3>player1 (leader)</h3>
 
-              <h3>player2</h3>
+      //         <h3>player2</h3>
 
-              <h3>player3</h3>
+      //         <h3>player3</h3>
 
-              <h3>player4</h3>
-            </div>
-            <button className="button-49">more Info</button>
-          </div>
-        </div>
-      </div>
-      <div id="#Ccard" className="Ccard">
-        <div className="EventHeading">Event Name</div>
-        <div className="Eventtime"> 12 Nov 2022</div>
-        <div className="layer">
-          <div className="teams">
-            <div className="teamname">
-              <h3>player1 (leader)</h3>
+      //         <h3>player4</h3>
+      //       </div>
+      //       <button className="button-49">more Info</button>
+      //     </div>
+      //   </div>
+      // </div>
+      // <div id="#Ccard" className="Ccard">
+      //   <div className="EventHeading">Event Name</div>
+      //   <div className="Eventtime"> 12 Nov 2022</div>
+      //   <div className="layer">
+      //     <div className="teams">
+      //       <div className="teamname">
+      //         <h3>player1 (leader)</h3>
 
-              <h3>player2</h3>
+      //         <h3>player2</h3>
 
-              <h3>player3</h3>
+      //         <h3>player3</h3>
 
-              <h3>player4</h3>
-            </div>
-            <button className="button-49">more Info</button>
-          </div>
-        </div>
-      </div>
-      <div id="#Ccard" className="Ccard">
-        <div className="EventHeading">Event Name</div>
-        <div className="Eventtime"> 12 Nov 2022</div>
-        <div className="layer">
-          <div className="teams">
-            <div className="teamname">
-              <h3>player1 (leader)</h3>
+      //         <h3>player4</h3>
+      //       </div>
+      //       <button className="button-49">more Info</button>
+      //     </div>
+      //   </div>
+      // </div>
+      // <div id="#Ccard" className="Ccard">
+      //   <div className="EventHeading">Event Name</div>
+      //   <div className="Eventtime"> 12 Nov 2022</div>
+      //   <div className="layer">
+      //     <div className="teams">
+      //       <div className="teamname">
+      //         <h3>player1 (leader)</h3>
 
-              <h3>player2</h3>
+      //         <h3>player2</h3>
 
-              <h3>player3</h3>
+      //         <h3>player3</h3>
 
-              <h3>player4</h3>
-            </div>
-            <button className="button-49">more Info</button>
-          </div>
-        </div>
-      </div>
+      //         <h3>player4</h3>
+      //       </div>
+      //       <button className="button-49">more Info</button>
+      //     </div>
+      //   </div>
+      // </div>
 
             
+      //   </div>
+      <main class="page-content">
+        
+       
+       
+        <div class="card">
+          <div class="content">
+            <h2 class="title">Explore The Galaxy</h2>
+            <p class="copy">Seriously, straight up, just blast off into outer space today</p>
+            <button class="btn">Book Now</button>
+          </div>
         </div>
+        <div class="card">
+          <div class="content">
+            <h2 class="title">Explore The Galaxy</h2>
+            <p class="copy">Seriously, straight up, just blast off into outer space today</p>
+            <button class="btn">Book Now</button>
+          </div>
+        </div>
+        <div class="card">
+          <div class="content">
+            <h2 class="title">Explore The Galaxy</h2>
+            <p class="copy">Seriously, straight up, just blast off into outer space today</p>
+            <button class="btn">Book Now</button>
+          </div>
+        </div>
+        <div class="card">
+          <div class="content">
+            <h2 class="title">Explore The Galaxy</h2>
+            <p class="copy">Seriously, straight up, just blast off into outer space today</p>
+            <button class="btn">Book Now</button>
+          </div>
+        </div>
+        <div class="card">
+          <div class="content">
+            <h2 class="title">Explore The Galaxy</h2>
+            <p class="copy">Seriously, straight up, just blast off into outer space today</p>
+            <button class="btn">Book Now</button>
+          </div>
+        </div>
+      </main>
 
     )
 }

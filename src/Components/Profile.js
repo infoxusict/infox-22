96467b5e31a59ae26bb9f5ff106ddbc1bfe166b3
Suraj Mishra <59327661+suraj-git07@users.noreply.github.com>
2022-10-30 21:54:35 +0530

    auth undef check added

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Assets/Images/CSS/Home.css";
 import MatrixRain from "./MatrixRain";
 // import Cards from "./ScheduleCards";
-// import EventCardProfile from "./EventProfile";
+import EventCardProfile from "./EventProfile";
 import "./Assets/Images/CSS/profile.css";
 import ProfileCard from "./ProfileCard";
 import jwt_decode from "jwt-decode";
@@ -28,20 +28,42 @@ const modalObject = [
 
 const Profile = () => {
   const [profileDATA, setprofileDATA] = React.useState({});
-  // const [isShown, setIsShown] = useState(false);
-  // const [isModalShown, setIsModalShown] = useState(false);
+  const [isModalShown, setIsModalShown] = useState(false);
   const [isAuthKey, setAuthKey] = useState(false);
 
-  useEffect(() => {
-    // eslint-disable-next-line
-  }, []);
+  //function to check whether user is signed up already
+  const ifSignIn = async () => {
+    var x = localStorage.getItem("authkey");
+    // console.log(userDetails);
+    if (x != null && x != undefined) {
+      const getDetailsRes = await fetch(
+        "https://infoxpression.herokuapp.com/user/getDetails",
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+            authToken: localStorage.getItem("authkey"),
+          },
+          referrerPolicy: "origin-when-cross-origin",
+        }
+      );
+      var finaldetailsres = await getDetailsRes.json();
+      userDetails = finaldetailsres;
+      setprofileDATA(finaldetailsres);
+      setAuthKey(true);
+      return true;
+    }
+    return false;
+  };
 
   const handleCallbackResponse = async (response) => {
+    // getting the jwt token and setting userObject as it response
     console.log("JWT ID TOKEN: ", response.credential);
     var userObject = await jwt_decode(response.credential);
 
-    // console.log(tempuserObject);
+    // check is google btn click then doing some stuff
     if (userObject.email_verified) {
+      // setting the res data to temuserObject
       tempuserObject = {
         name: userObject.name,
         email: userObject.email,
@@ -52,6 +74,14 @@ const Profile = () => {
         contact: null,
         gradYear: null,
       };
+
+      await ifSignIn();
+      //setting to show modal
+      setTimeout(() => {
+        setIsModalShown(true);
+      }, 1600);
+
+      // request to post data to db
       const resCheck = await fetch(
         "https://infoxpression.herokuapp.com/user/auth/google/chk",
         {
@@ -65,8 +95,11 @@ const Profile = () => {
       );
       var checkres = await resCheck.json();
       console.log(checkres);
-      if (checkres.success) {
+      // if reponse of posting data is success, then setting the auth key to local storage
+      if (checkres.success && checkres.authKey != undefined) {
         localStorage.setItem("authkey", checkres.authKey);
+
+        // as we get the auth key we can fetch the data from db
         const getDetailsRes = await fetch(
           "https://infoxpression.herokuapp.com/user/getDetails",
           {
@@ -99,28 +132,6 @@ const Profile = () => {
       theme: "outline",
       size: "large",
     });
-    const ifSignIn = async () => {
-      var x = localStorage.getItem("authkey");
-      console.log(userDetails);
-      if (x != null) {
-        const getDetailsRes = await fetch(
-          "https://infoxpression.herokuapp.com/user/getDetails",
-          {
-            method: "POST",
-            headers: {
-              "Content-type": "application/json",
-              authToken: localStorage.getItem("authkey"),
-            },
-            referrerPolicy: "origin-when-cross-origin",
-          }
-        );
-        var finaldetailsres = await getDetailsRes.json();
-        userDetails = finaldetailsres;
-        setprofileDATA(finaldetailsres);
-        // console.log(finaldetailsres);
-        setAuthKey(true);
-      }
-    };
 
     ifSignIn();
     // eslint-disable-next-line
@@ -146,13 +157,13 @@ const Profile = () => {
             style={{
               fontFamily: "atmospheric",
               color: "#6CDE01",
-              paddingBottom: "1vw",
-              margin: "0vw 9vw 2vw 26vw",
+              // paddingBottom: "1vw",
+              // margin: "0vw 9vw 2vw 26vw",
             }}
           >
-            Get Started
+            {!isAuthKey ? "Get Started" : "Register Events"}
           </div>
-          {!isAuthKey ? (
+          {isModalShown && !isAuthKey ? (
             <Modal
               data={modalObject}
               buttonName="Complete Registration"
@@ -167,7 +178,7 @@ const Profile = () => {
           )}
           {isAuthKey ? (
             <>
-              {/* <EventCardProfile /> */}
+              <EventCardProfile />
               <div className="profilePart">
                 <ProfileCard
                   name={profileDATA.name}

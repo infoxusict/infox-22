18d2a2cf77d19f345113c4b95628b9a3a685ff82
Dhruv Aeron <dhruv.aeron3@gmail.com>
2022-11-05 21:28:29 +0530

    schedule responsive

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -29,47 +29,56 @@ function Event() {
   }, []);
 
   return (
-    <div className=" flex flex-wrap justify-evenly md:mt-24">
+    <>
       <MatrixRain />
-      <h1
-        className="fn_title theme HeadH3Res pb-9"
-        data-wait={1000}
-        data-speed={8}
-        style={{ width: "100vw" }}
-      >
-        Events
-      </h1>
-      {events &&
-        events.map((event) => (
-          <div className="sch-event-card">
-            <div className="sch-event-pic">
-              <img src={event.eventPic} className="sch-event-img" alt="" />
-            </div>
-            <div className="sch-event-info">
-              <h2 className="sch-event-name">{event.eventName}</h2>
-              <div className="sch-date-venue">
-                <h3 className="sch-event-date">{event.date}, E block</h3>
-                {/* <h3 className='sch-event-venue'>E-Block </h3> */}
+      <div className=" flex flex-wrap justify-evenly mt-20 md:mt-24">
+        <div class="sn_glitch_forNHeading atmosphere uh-heading  ">
+          <div class="sn_line_forNHeading">Events</div>
+          <div class="sn_line_forNHeading">Events</div>
+          <div class="sn_line_forNHeading">Events</div>
+          <div class="sn_line_forNHeading">Events</div>
+          <div class="sn_line_forNHeading">Events</div>
+          <div class="sn_line_forNHeading">Events</div>
+          <div class="sn_line_forNHeading">Events</div>
+          <div class="sn_line_forNHeading">Events</div>
+          <div class="sn_line_forNHeading">Events</div>
+        </div>
+        <div className="grid md:grid-cols-2 grid-col-1">
+          {events &&
+            events.map((event) => (
+              <div className="sch-event-card">
+                <div className="sch-event-pic">
+                  <img src={event.eventPic} className="sch-event-img" alt="" />
+                </div>
+                <div className="sch-event-info">
+                  <h2 className="sch-event-name mt-6 text-2xl  atmosphere">
+                    {event.eventName}
+                  </h2>
+                  <div className="sch-date-venue">
+                    <h3 className="sch-event-date">{event.date}, E block</h3>
+                    {/* <h3 className='sch-event-venue'>E-Block </h3> */}
+                  </div>
+                  <h3 className="sch-event-desc line-clamp-1">
+                    {event.description[0].split("", 150)}...
+                  </h3>
+                  <button className="mt-12 mb-4">
+                    <a
+                      href={"http://localhost:3000/event/" + event.eventId}
+                      className="register teams-btn text-xs "
+                    >
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      Visit Event
+                    </a>
+                  </button>
+                </div>
               </div>
-              <h3 className="sch-event-desc line-clamp-1">
-                {event.description[0].split("", 150)}...
-              </h3>
-              <button>
-                <a
-                  href={"http://localhost:3000/event/" + event.eventId}
-                  className="register team-btn sch-event-team-btn"
-                >
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  Visit Event
-                </a>
-              </button>
-            </div>
-          </div>
-        ))}
-    </div>
+            ))}
+        </div>
+      </div>
+    </>
   );
 }
 

edab5ee008e89b1bb0dd4338a432e3bf000cd3d5
Suraj Mishra <59327661+suraj-git07@users.noreply.github.com>
2022-11-08 13:24:27 +0530

    logout button added

--- src/Components/ProfileCard.js
+++ src/Components/ProfileCard.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import { useEffect } from "react/cjs/react.production.min";
+import { useHistory } from "react-router-dom";
 
 export default function ProfileCard(props) {
+  const [isAuthKey, setisAuthKey] = useState(false);
+
+  const history = useHistory();
+  const ifSignIn = async () => {
+    var x = localStorage.getItem("authkey");
+    if (x != null && x !== undefined) {
+      setisAuthKey(true);
+    }
+    setisAuthKey(false);
+  };
+
+  const logoutUser = () => {
+    localStorage.removeItem("authkey");
+    console.log("btnclick");
+    setisAuthKey(false);
+    history.push("/");
+  };
+
+  useEffect(() => {
+    ifSignIn();
+  }, []);
 
   return (
     <div className="profileCard">
@@ -9,12 +32,23 @@ export default function ProfileCard(props) {
         src="https://infoxpression.tech/static/media/aswd.a0dd4d343e8754fcfb94.png"
         alt=""
       />
-      <div className="profilebox"></div>
-      <div className="profilepic"  
-       style={{}}
-       > 
-        <img className="headshot headshot-2"
-        src={props.image} ></img>
+
+      <div className="profilebox">
+        <div>
+          {!isAuthKey ? (
+            <button id="logoutbtn" onClickCapture={logoutUser}>
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/126/126467.png"
+                className="logoutimg"
+              />
+            </button>
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+      <div className="profilepic" style={{}}>
+        <img className="headshot headshot-2" src={props.image}></img>
       </div>
       <div className="profileData">
         <h3>{props.name} </h3>

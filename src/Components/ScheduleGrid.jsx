66754006e75ae97d74d9f7f5f56b2bea8b9d6ac4
Ayush <ayush4002gupta@gmail.com>
2022-11-08 19:36:16 +0530

    pushing schedule

--- src/Components/ScheduleGrid.jsx
+++ src/Components/ScheduleGrid.jsx
@@ -4,6 +4,7 @@ import MatrixRain from "../Components/MatrixRain"
 
 function ScheduleGrid() {
   return (
+    <>
     <div className='sch-par-div'>
       <MatrixRain/>
       <div class="angry-grid">
@@ -50,6 +51,105 @@ function ScheduleGrid() {
 
 
     </div>
+    <h1>
+ <div className='day2'>DAY-2</div></h1>
+    <div className='sch-par-div'>
+      <MatrixRain/>
+      <div class="angry-grid-1">
+        {/* <div className='sch-grid-event' id="item-0">&nbsp;0</div> */}
+        <div className='sch-grid-event' id="item-1">E-block Audi</div>
+        <div className='sch-grid-event' id="item-2-1">E-218-319</div>
+        <div className='sch-grid-event' id="item-3">&nbsp;10:00</div>
+        <div className='sch-grid-event' id="item-4">&nbsp;11:00</div>
+        <div className='sch-grid-event' id="item-5">&nbsp;12:00</div>
+        <div className='sch-grid-event' id="item-6">&nbsp;13:00</div>
+        <div className='sch-grid-event' id="item-7">&nbsp;14:00</div>
+        <div className='sch-grid-event' id="item-8">&nbsp;15:00</div>
+        <div className='sch-grid-event' id="item-9">&nbsp;16:00</div>
+        <div className='sch-grid-event' id="item-10">&nbsp;17:00</div>
+        <div className='sch-grid-event' id="item-11">&nbsp;18:00</div>
+        <div className='sch-grid-event' id="item-12">&nbsp;19:00</div>
+      
+      {/*<div className='sch-grid-event' id="item-13">&nbsp;Inauguration</div>*/}
+        <div className='sch-grid-event' id="item-14">&nbsp;Fill in the code</div>
+        <div className='sch-grid-event' id="item-15-1">&nbsp;Battle of Codes</div>
+        <div className='sch-grid-event' id="item-16">Library Front</div>
+        <div className='sch-grid-event' id="item-17-1">&nbsp;Robo War</div>
+        <div className='sch-grid-event' id="item-18">E 312-313</div>
+        <div className='sch-grid-event' id="item-19-1">&nbsp;NFT Rush</div>
+        <div className='sch-grid-event' id="item-20-1">&nbsp;Bidding Loop</div>
+        <div className='sch-grid-event' id="item-21-1">Community Hall</div>
+        <div className='sch-grid-event' id="item-22-1">&nbsp;Robo Soccer</div>
+        <div className='sch-grid-event' id="item-23">&nbsp;20:00</div>
+        <div className='sch-grid-event' id="item-24">&nbsp;21:00</div>
+        <div className='sch-grid-event' id="item-25">&nbsp;22:00</div>
+        <div className='sch-grid-event' id="item-26">C-block Audi</div>
+        {/* <div className='sch-grid-event' id="item-27">&nbsp;17:00</div> */}
+        <div className='sch-grid-event' id="item-28-1">&nbsp;Cryptomaniacs</div>
+        {/* <div className='sch-grid-event' id="item-29">&nbsp;29</div> */}
+        {/* <div className='sch-grid-event' id="item-30">&nbsp;30</div> */}
+        {/* <div className='sch-grid-event' id="item-31">&nbsp;17:30</div>
+        <div className='sch-grid-event' id="item-32">&nbsp;18:00</div>
+        <div className='sch-grid-event' id="item-33">&nbsp;18:30</div>
+        <div className='sch-grid-event' id="item-34">&nbsp;19:00</div>
+        <div className='sch-grid-event' id="item-35">&nbsp;19:30</div> */}
+        <div className='sch-grid-event' id="item-36">&nbsp;Labs</div>
+        {/* <div className='sch-grid-event' id="item-37">&nbsp;37</div> */}
+       {/* <div className='sch-grid-event' id="item-38">&nbsp;CTF</div>*/}
+       
+      </div>
+    </div>
+   <h1> <div className='day2'>DAY-3</div></h1>
+   <div className='sch-par-div'>
+      <MatrixRain/>
+      <div class="angry-grid">
+        {/* <div className='sch-grid-event' id="item-0">&nbsp;0</div> */}
+        <div className='sch-grid-event' id="item-1">&nbsp;E-block Audi</div>
+        <div className='sch-grid-event' id="item-2">&nbsp;E-218-219</div>
+        <div className='sch-grid-event' id="item-3">&nbsp;10:00</div>
+        <div className='sch-grid-event' id="item-4">&nbsp;11:00</div>
+        <div className='sch-grid-event' id="item-5">&nbsp;11:30</div>
+        <div className='sch-grid-event' id="item-6">&nbsp;12:00</div>
+        <div className='sch-grid-event' id="item-7">&nbsp;12:30</div>
+        <div className='sch-grid-event' id="item-8">&nbsp;13:00</div>
+        <div className='sch-grid-event' id="item-9">&nbsp;13:30</div>
+        <div className='sch-grid-event' id="item-10">&nbsp;14:00</div>
+        <div className='sch-grid-event' id="item-11">&nbsp;14:30</div>
+        <div className='sch-grid-event' id="item-12">&nbsp;15:00</div>
+        <div className='sch-grid-event' id="item-13-2">&nbsp;Ad Mad</div>
+        <div className='sch-grid-event' id="item-14">&nbsp;Unraveller (Data Breach)</div>
+        <div className='sch-grid-event' id="item-15-2">&nbsp;Duplicate</div>
+        <div className='sch-grid-event' id="item-16">&nbsp;E 109</div>
+        <div className='sch-grid-event' id="item-17-2">&nbsp;Treasure Hunt</div>
+        <div className='sch-grid-event' id="item-18">&nbsp;C Block Audi</div>
+        <div className='sch-grid-event' id="item-19-2">&nbsp;Shark Tank</div>
+        <div className='sch-grid-event' id="item-20-2">&nbsp;Clash of Companies</div>
+        {/*<div className='sch-grid-event' id="item-21">&nbsp;Canteen Front</div>
+        <div className='sch-grid-event' id="item-22">&nbsp;Robo Soccer</div>*/}
+        <div className='sch-grid-event' id="item-23">&nbsp;15:30</div>
+        <div className='sch-grid-event' id="item-24">&nbsp;16:00</div>
+        <div className='sch-grid-event' id="item-25">&nbsp;16:30</div>
+         {/*<div className='sch-grid-event' id="item-26">&nbsp;C-block Audi</div>*/}
+         <div className='sch-grid-event' id="item-27">&nbsp;17:00</div> 
+          {/*<div className='sch-grid-event' id="item-28">&nbsp;Gaming Event</div>*/}
+        {/* <div className='sch-grid-event' id="item-29">&nbsp;29</div> */}
+        {/* <div className='sch-grid-event' id="item-30">&nbsp;30</div> */}
+        {/* <div className='sch-grid-event' id="item-31">&nbsp;17:30</div>
+        <div className='sch-grid-event' id="item-32">&nbsp;18:00</div>
+        <div className='sch-grid-event' id="item-33">&nbsp;18:30</div>
+        <div className='sch-grid-event' id="item-34">&nbsp;19:00</div>
+        <div className='sch-grid-event' id="item-35">&nbsp;19:30</div> */}
+        <div className='sch-grid-event' id="item-36">&nbsp;E 312-313</div>
+        {/* <div className='sch-grid-event' id="item-37">&nbsp;37</div> */}
+        {/*<div className='sch-grid-event' id="item-38">&nbsp;CTF</div>*/}
+        <div className='sch-grid-event' id="item-39">&nbsp;Valedictory Ceremony</div>
+        
+
+      </div>
+
+
+    </div>
+    </>
   )
 }
 

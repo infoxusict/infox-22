e2c5c3c1a95f46cc6a56f074ff6f152c79de0229
Kashish Jain <9682284-Kashishjain04@users.noreply.gitlab.com>
2022-11-13 14:02:18 +0530

    banner text

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,18 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Banner() {
   return (
     <div className="glass-bg">
       <h3 className="banner-text text-xs md:text-lg md:pb-2">
-        Our Uhack 5.0 registrations are live.{" "}
-        <a
-          rel="noreferrer"
-          href="https://uhack5.devfolio.co/"
-          target="_blank"
+        Event registrations are live.{" "}
+        <Link
+          to="/events"
           className="form-link"
         >
           Click Here to Register
-        </a>
+        </Link>
       </h3>
     </div>
   );

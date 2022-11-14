a2ac0f06a1200d712c697292ed20a6136e6ca5ff
Dhruv Aeron <96003229+Aeron2@users.noreply.github.com>
2022-11-15 01:26:48 +0530

    Stable (#24)
    * sponsors added

--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -6,8 +6,235 @@ const Schedule = () => {
     <>
       <MatrixRain />
 
-      <div className="scheduleDiv" style={{ height: "80vh" }}>
-        <h2 style={{ fontSize: "5vh" }}>To Be Revealed...</h2>
+      <div className="grid grid-cols-2 md:grid-cols-4 justify-items-center items-center mx-3 gap-x-6 gap-y-12 md:gap-y-20 md:gap-x-12 mt-6 md:mt-14 uh-bg py-16 md:px-16">
+        <a
+          href="https://www.vardiano.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="basis-2/5 md:basis-1/5 spon-logo-width col-start-1 col-end-3 md:col-end-5"
+        >
+          <img src={require("./Assets/Images/sponsor-2/vardiano.png")} alt="" />
+        </a>
+        <a
+          href="https://polygon.technology/"
+          target="_blank"
+          rel="noreferrer"
+          className="basis-2/5 md:basis-1/5 spon-logo-width"
+        >
+          <img
+            src={require("./Assets/Images/images/uhack/Polygon-logo.png")}
+            alt=""
+          />
+        </a>
+        <a
+          href="https://devfolio.co"
+          target="_blank"
+          rel="noreferrer"
+          className="basis-2/5 md:basis-1/5 spon-logo-width"
+        >
+          <img
+            src={require("./Assets/Images/images/uhack/Devfolio-logo.png")}
+            alt=""
+          />
+        </a>
+        <a
+          href="https://www.aceenggacademy.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="basis-2/5 md:basis-1/5 spon-logo-width"
+        >
+          <img
+            src={require("./Assets/Images/sponsor-2/ace.png")}
+            alt="Devfolio-logo"
+          />
+        </a>
+        <a
+          href="https://filecoin.io"
+          target="_blank"
+          rel="noreferrer"
+          className="basis-2/5 md:basis-1/5 spon-logo-width"
+        >
+          <img
+            src={require("./Assets/Images/images/uhack/Filecoin-logo.png")}
+            alt="Filecoin-logo"
+          />
+        </a>
+        <a
+          href="/"
+          target="_blank"
+          rel="noreferrer"
+          className="basis-2/5 md:basis-1/5 spon-logo-width"
+        >
+          <img
+            src={require("./Assets/Images/sponsor-2/brocode.png")}
+            alt="Devfolio-logo"
+          />
+        </a>
+        <a
+          href="https://www.duolearn.tech/"
+          target="_blank"
+          rel="noreferrer"
+          className="basis-2/5 md:basis-1/5 spon-logo-width"
+        >
+          <img
+            src={require("./Assets/Images/sponsor-2/duolearnlogo.png")}
+            alt="Devfolio-logo"
+          />
+        </a>
+        <a
+          href="https://www.fossanalytics.com/en-in"
+          target="_blank"
+          rel="noreferrer"
+          className="basis-2/5 md:basis-1/5 spon-logo-width"
+        >
+          <img
+            src={require("./Assets/Images/sponsor-2/foss.png")}
+            alt="Devfolio-logo"
+          />
+        </a>
+        <a
+          href="hhttps://www.homversity.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="basis-2/5 md:basis-1/5 spon-logo-width"
+        >
+          <img
+            src={require("./Assets/Images/sponsor-2/homeversity.png")}
+            alt="Devfolio-logo"
+          />
+        </a>
+        <a
+          href="https://mathcody.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="basis-2/5 md:basis-1/5 spon-logo-width"
+        >
+          <img
+            src={require("./Assets/Images/sponsor-2/math and cody.png")}
+            alt="Devfolio-logo"
+          />
+        </a>
+        <a
+          href="/"
+          target="_blank"
+          rel="noreferrer"
+          className="basis-2/5 md:basis-1/5 spon-logo-width"
+        >
+          <img
+            src={require("./Assets/Images/sponsor-2/o_hi_app.png")}
+            alt="Devfolio-logo"
+          />
+        </a>
+        <a
+          href="https://pearlacademy.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="basis-2/5 md:basis-1/5 spon-logo-width"
+        >
+          <img
+            src={require("./Assets/Images/sponsor-2/pearl academy.png")}
+            alt="Devfolio-logo"
+          />
+        </a>
+        <a
+          href="https://unacademy.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="basis-2/5 md:basis-1/5 spon-logo-width"
+        >
+          <img
+            src={require("./Assets/Images/sponsor-2/unacademy (2).png")}
+            alt="Devfolio-logo"
+          />
+        </a>
+        <a
+          href="https://replit.com"
+          target="_blank"
+          rel="noreferrer"
+          className="md:basis-1/5 spon-logo-width basis-2/5"
+        >
+          <img
+            src={require("./Assets/Images/images/uhack/Replit-logo.png")}
+            alt="Replit-logo"
+          />
+        </a>
+        <a
+          href="https://www.digitalocean.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="md:basis-1/5 spon-logo-width basis-2/5"
+        >
+          <img
+            src={require("./Assets/Images/images/uhack/Do-logo.png")}
+            alt="Do-logo"
+          />
+        </a>
+        <a
+          href="https://gen.xyz/"
+          target="_blank"
+          rel="noreferrer"
+          className="md:basis-1/5 wolfram-logo basis-2/5"
+        >
+          <img
+            src={require("./Assets/Images/images/uhack/xyz-logo.png")}
+            alt="xyz-logo"
+          />
+        </a>
+        <a
+          href="https://solana.com"
+          target="_blank"
+          rel="noreferrer"
+          className="md:basis-1/5 spon-logo-width basis-2/5"
+        >
+          <img
+            src={require("./Assets/Images/images/uhack/Solana-logo.png")}
+            alt="Solana-logo"
+          />
+        </a>
+        <a
+          href="https://wolfram.com"
+          target="_blank"
+          rel="noreferrer"
+          className="md:basis-1/5 spon-logo-width basis-2/5"
+        >
+          <img
+            src={require("./Assets/Images/images/uhack/wolfram-alpha-logo.png")}
+            alt="Wolfram-logo"
+          />
+        </a>
+        <a
+          href="https://www.echo3d.co/"
+          target="_blank"
+          rel="noreferrer"
+          className="md:basis-1/5 spon-logo-width basis-2/5"
+        >
+          <img
+            src={require("./Assets/Images/images/uhack/echo-logo.png")}
+            alt="echo-logo"
+          />
+        </a>
+        <a
+          href="https://www.taskade.com/"
+          target="_blank"
+          rel="noreferrer"
+          className=" spon-logo-width "
+        >
+          <img
+            src={require("./Assets/Images/images/uhack/taskade-logo.png")}
+            alt="taskade-logo "
+          />
+        </a>{" "}
+        <a
+          href="https://edtimes.in/"
+          target="_blank"
+          rel="noreferrer"
+          className="basis-2/5 md:basis-1/5 wolfram-logo"
+        >
+          <img
+            src={require("./Assets/Images/sponsor-2/ed times.png")}
+            alt="Devfolio-logo"
+          />
+        </a>
       </div>
     </>
   );
--- src/Components/SponsersNew.js
+++ src/Components/SponsersNew.js
@@ -1,31 +1,47 @@
-import Aos from "aos"
-import Schedule from './Schedule'
-import { useEffect } from "react"
+import Aos from "aos";
+import Schedule from "./Schedule";
+import { useEffect } from "react";
 
 export default function Sponser() {
-
-  useEffect(()=>{
-    Aos.init({duration:2000})
-  })
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  });
   return (
     <section>
-
-      <div className='sponsers' data-aos="fade-up">
-      <div className="sn_glitch_forNHeading atmosphere snsponsors">
-            <div className="sn_line_forNHeading">SPONSORS</div>
-            <div className="sn_line_forNHeading">SPONSORS</div>
-            <div className="sn_line_forNHeading">SPONSORS</div>
-            <div className="sn_line_forNHeading">SPONSORS</div>
-            <div className="sn_line_forNHeading">SPONSORS</div>
-            <div className="sn_line_forNHeading">SPONSORS</div>
-            <div className="sn_line_forNHeading">SPONSORS</div>
-            <div className="sn_line_forNHeading">SPONSORS</div>
-            <div className="sn_line_forNHeading">SPONSORS</div>
+      <div className="sponsers" data-aos="fade-up">
+        <div className="sn_glitch_forNHeading atmosphere snsponsors flex items-center justify-center">
+          <div className="sn_line_forNHeading flex items-center justify-center">
+            SPONSORS
           </div>
+          <div className="sn_line_forNHeading flex items-center justify-center">
+            SPONSORS
+          </div>
+          <div className="sn_line_forNHeading flex items-center justify-center">
+            SPONSORS
+          </div>
+          <div className="sn_line_forNHeading flex items-center justify-center">
+            SPONSORS
+          </div>
+          <div className="sn_line_forNHeading flex items-center justify-center">
+            SPONSORS
+          </div>
+          <div className="sn_line_forNHeading flex items-center justify-center">
+            SPONSORS
+          </div>
+          <div className="sn_line_forNHeading flex items-center justify-center">
+            SPONSORS
+          </div>
+          <div className="sn_line_forNHeading flex items-center justify-center">
+            SPONSORS
+          </div>
+          <div className="sn_line_forNHeading flex items-center justify-center">
+            SPONSORS
+          </div>
+        </div>
         <div className="sponsers-whole">
-        <Schedule />
+          <Schedule />
         </div>
       </div>
     </section>
-  )
+  );
 }
